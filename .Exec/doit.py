ad66503262d06da3be843911c26fa5dcd50e7468
Aalok <codeaalok@gmail.com>
2023-03-22 20:28:21 +0000

    added links

--- .Exec/doit.py
+++ .Exec/doit.py
@@ -44,7 +44,7 @@ def action(ch):
 		print("Please wait for ",rand_number,"seconds")
 		for i in range(rand_number):
 			time.sleep(1)
-			print(Fore.YELLOW+str(i),sep=" ")
+			print(Fore.YELLOW+str(i),end=" ")
 #			progressBar(i,rand_number)
 #		time.sleep(rand_number)
 		print(Fore.GREEN+"All done. Go to your website to verify changes.")
